977f7e339b249927efdaa156a148f2af9ee25d07
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-05-11 23:38:17 -0500

    se crearon dos funciones para consumir datos de paises

--- src/__test__/fixtures/countries.js
+++ src/__test__/fixtures/countries.js
@@ -1,26 +1,50 @@
 const fetch = require("node-fetch");
-const fs = require('fs')
 
 //datos de los países de: https://restcountries.eu/
 let countries = [];
 
-fetch("https://restcountries.eu/rest/v2/all")
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        for (const country of data) {
-            countries.push({
-                code: country.alpha2Code,
-                name: country.name,
-                capital: country.capital,
-                spanishName: country.translations.es,
+function getCountries() {
+
+    fetch("https://restcountries.eu/rest/v2/all")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            for (const country of data) {
+                countries.push({
+                    code: country.alpha2Code,
+                    name: country.name,
+                    capital: country.capital,
+                    spanishName: country.translations.es,
+                    population: Math.round(Math.random() * 1000000),
+                    beds: Math.round(Math.random() * 10000)
+                });
+            }
+            return countries
+        })
+}
+
+function getCountry(isoCode) {
+
+    fetch(`https://restcountries.eu/rest/v2/alpha/${isoCode}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let respuesta = {
+                code: data.alpha2Code,
+                name: data.name,
+                capital: data.capital,
+                spanishName: data.translations.es,
                 population: Math.round(Math.random() * 1000000),
                 beds: Math.round(Math.random() * 10000)
-            });
-        }
-        fs.writeFile('./db.json',
-            JSON.stringify(countries), 'utf8', (err) => {
-                console.log('El archivo ha sido creado');
-            })
-    });
+            }
+            return respuesta
+        })
+
+}
+
+export default {
+    getCountries,
+    getCountry
+}
