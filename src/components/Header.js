1eec126e076c531e8e91508e79f72cd8d93fa9d3
David Perez <dperezval08@gmail.com>
2020-05-20 20:08:13 -0500

    Opciones del header quitadas

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,6 @@ const Header = (props) => {
         <img className="header-logo__image" src={Logo} alt="Logo" />
         <h1 className="header-logo__title">CoronaData</h1>
       </figure>
-      <menu className="header-menu">
-        <li className="header-menu__option">Texto</li>
-        <li className="header-menu__option">Texto</li>
-        <li className="header-menu__option">Texto</li>
-        <li className="header-menu__option">Texto</li>
-      </menu>
     </header>
   );
 };
