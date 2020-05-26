60f5dcedea56c5d8114fd8b1ebeebb039d34c922
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-05-26 10:55:40 -0500

    Se ha añadido el logotipo del proyecto en formato SVG

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles/Header.css";
-import Logo from "../images/LogoCoronaData.png";
+import { Logo } from './Logo'
 
 const Header = (props) => {
   return (
     <header className="header">
       <figure className="header-logo">
-        <img className="header-logo__image" src={Logo} alt="Logo" />
+      <Logo className="header-logo__image"/>
+        {/* <img className="header-logo__image" src={Logo} alt="Logo" /> */}
         <h1 className="header-logo__title">CoronaData</h1>
       </figure>
     </header>
